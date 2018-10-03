da9f403aa60a97889d2bd4a77c5574b4989c9b99
Adam Smrekar <adsm4243@colorado.edu>
2018-10-03 00:53:23 -0600

    Add files via upload

---
+++ Master/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 3, 2017
+ *      Author: Brandon Lewien, Adam Smrekar
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void timerA0_config();
+
+#endif /* TIMER_H_ */
